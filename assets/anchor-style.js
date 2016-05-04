11628ce83b4781f013dfd168889d8ad185ca5fda
Simran-B <simran@arangodb.com>
2016-05-04 22:32:32 +0200

    Allow plugin configuration

--- assets/anchor-style.js
+++ assets/anchor-style.js
@@ -1,8 +1,7 @@
-anchors.options = {
-	icon: '#'
-	,placement: 'right'
-}
+anchors.options =
+	this.options.pluginsConfig.headingAnchors ||
+	{icon: '#',	placement: 'right'};
 
-gitbook.events.bind('page.change', function(){
+gitbook.events.bind('page.change', function() {
 	anchors.add('h1,h2,h3,h4,h5')
-})
+});
